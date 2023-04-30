47eefb547e39a966b1fa266f10f4554a803e54d6
Ganmo3 <131627788+Ganmo3@users.noreply.github.com>
2023-04-30 21:37:34 +0000

    [Add] ひな形

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,9 +4,12 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to '/top'
+   @book = Book.new(book_params)
+   if @book.save
+    redirect_to books_path(@book.id)
+  else
+    render :new
+  end
   end
 
   def index
@@ -14,11 +17,26 @@ class BooksController < ApplicationController
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    #変更必要あり
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to books_path(book.id)  
   end
   
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
+  end
+
   private
   def book_params
     params.require(:book).permit(:title, :body)
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   post 'books' => 'books#create'
   get 'books' => 'books#index'
   get 'books/show'
-  get 'books/edit'
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
   get '/top' => 'homes#top'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'books/:id' => 'books#show'
+  #変更？
+  patch 'books/:id' => 'books#update', as: 'update_book'
+  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
 end

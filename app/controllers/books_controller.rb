80e7b5b2ae21a7e55bfc05a104b7196a427cdc43
Ganmo3 <131627788+Ganmo3@users.noreply.github.com>
2023-05-01 12:58:32 +0000

    [Add] 大枠完成（next レイアウト)

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -23,12 +23,11 @@ class BooksController < ApplicationController
   end
 
   def update
-    #変更必要あり
     book = Book.find(params[:id])
     book.update(book_params)
-    redirect_to book_path(book.id)  
+    redirect_to books_path(book.id)
   end
-  
+
   def destroy
     book = Book.find(params[:id])
     book.destroy
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,6 @@ Rails.application.routes.draw do
   get 'books/:id/edit' => 'books#edit', as: 'edit_book'
   get '/top' => 'homes#top'
   get 'books/:id' => 'books#show'
-  #変更？
   patch 'books/:id' => 'books#update', as: 'update_book'
   delete 'books/:id' => 'books#destroy', as: 'destroy_book'
 end

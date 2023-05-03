981e9d721d19217cb64d34f103fb708f304d9ea4
Ganmo3 <131627788+Ganmo3@users.noreply.github.com>
2023-05-03 20:44:54 +0000

    [fix] ルートパス設定

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
   patch 'books/:id' => 'books#update', as: 'update_book'
   delete 'books/:id' => 'books#destroy', as: 'destroy_book'
   get '/' => 'homes#top'
+  root :to => 'homes#top'
 end

0501c6e8f128187dbe3da9f29c9042a73b22f56a
Ganmo3 <131627788+Ganmo3@users.noreply.github.com>
2023-05-03 13:41:47 +0000

    [Add] 28 update失敗バリデーション実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,10 +4,14 @@ class BooksController < ApplicationController
   end
 
   def create
-   book = Book.new(book_params)
-   book.save
-   flash[:notice] = "Book was successfully created."
-   redirect_to "/books/#{book.id}"
+   @book = Book.new(book_params)
+   if @book.save
+    flash[:notice] = "Book was successfully created."
+    redirect_to book_path(@book.id)
+   else
+    @books = Book.all
+    render :index
+   end
   end
 
   def index
@@ -24,14 +28,19 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to books_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+     flash[:notice] = "Book was successfully updated."
+     redirect_to book_path(@book.id)
+   else
+     render :edit
+   end
   end
 
   def destroy
     book = Book.find(params[:id])
     book.destroy
+    flash[:notice] = "Book was successfully destroyed."
     redirect_to '/books'
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,9 @@ Rails.application.routes.draw do
   get 'books/new'
   post 'books' => 'books#create'
   get 'books' => 'books#index'
-  get 'books/show'
   get 'books/:id/edit' => 'books#edit', as: 'edit_book'
-  get '/top' => 'homes#top'
-  get 'books/:id' => 'books#show'
+  get 'books/:id' => 'books#show', as: 'book'
   patch 'books/:id' => 'books#update', as: 'update_book'
   delete 'books/:id' => 'books#destroy', as: 'destroy_book'
+  get '/' => 'homes#top'
 end
